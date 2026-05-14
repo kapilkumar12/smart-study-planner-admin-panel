d02dccafd8ac2c0c6e7adff7b8e825addaab7f51
kapilkumar12 <kapilkumar1492003@gmail.com>
2026-05-14 15:07:20 +0530

    add live api link

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-study-planner-backend-wb14.onrender.com/api",
   withCredentials: true,
 });
 
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://smart-study-planner-backend-wb14.onrender.com", {
   withCredentials: true,
 });
 
